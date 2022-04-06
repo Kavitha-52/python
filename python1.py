efbc49bd73b603c10ac6b3d1492029b98b812005
kavitha <kavitha.n@thundersoft.com>
2022-04-06 09:30:04 +0530

    print(a) a=b=c=20 print(a,b,c) a=30 print(a,b,c) print(c,b,a) x="hii" y="hello" print(x+y) k="welcome" print(k) a,b,c,d=10,20.234,"thundersoft","ts" print(a,b,c,d) def m1():     a=10     print(a)     return def m2():     a=20     print(a)     return m1() m2() def m1():     a=10     print(a)     return def m2(a):
        print(a)
        return
    m1()
    m2(30)
    a=20
    def test():
        global a
        a=a+10
        print(a)
        return
    test()
    print(a)
    a=20
    def test():  # this program gives error
        a=a+10
        print(a)
        return
    a=2+2
    print(a)
    a=10
    float(a)
    id(1+1)
    5>3 and 5==5'''
    a=1
    b=10
    list=[11,2,3,4,5]
    if(a in list):
        print("a is available in list")
    else:
        print("a is not available in list")

---
+++ python1.py
@@ -0,0 +1,65 @@
+
+'''a=10
+print(a)
+a=b=c=20
+print(a,b,c)
+a=30
+print(a,b,c)
+print(c,b,a)
+x="hii"
+y="hello"
+print(x+y)
+k="welcome"
+print(k)
+a,b,c,d=10,20.234,"thundersoft","ts"
+print(a,b,c,d)
+def m1():
+    a=10
+    print(a)
+    return
+def m2():
+    a=20
+    print(a)
+    return
+m1()
+m2()
+def m1():
+    a=10
+    print(a)
+    return
+def m2(a):
+    
+    print(a)
+    return
+m1()
+m2(30)
+a=20
+def test():
+    global a
+    a=a+10
+    print(a)
+    return
+test()
+print(a)
+a=20
+def test():  # this program gives error
+    a=a+10
+    print(a)
+    return
+a=2+2
+print(a)
+a=10
+float(a)
+id(1+1)
+5>3 and 5==5'''
+a=1
+b=10
+list=[11,2,3,4,5]
+if(a in list):
+    print("a is available in list")
+else:
+    print("a is not available in list")
+          
+
+
+
