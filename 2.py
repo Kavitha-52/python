2c13575ef1968448813943c76e42f0ccce74d7e7
Kavitha-52 <98574222+Kavitha-52@users.noreply.github.com>
2022-05-16 23:58:23 +0530

    python

---
+++ 2.py
@@ -0,0 +1,54 @@
+'''a=10
+print(a)
+a=b=c=20
+print(a,b,c)
+a=30
+print(a,b,c)
+print(c,b,a)
+print(a+b+c)
+x="hii"
+y="hello"
+print(x+y)
+a=10
+#print(a)
+a='welcome'
+print(a)
+a,b,c,d=10,20.222,"thundersoft",'TS'
+print(a,b,c,d)
+print(d,c,b,a)
+a=10
+float(a)
+def m1():
+    a=10
+    print(a)
+    return
+def m2():
+    a=20
+    print(a)
+    return
+m1()
+m2()
+def m1():
+    a=10
+    print(a)
+    return
+def m2(a):
+    print(a)
+    return
+m1()
+m2(20)
+a=20
+def test():
+    global a
+    a=a+10
+    print(a)
+    return
+test()
+print(a)
+a=20
+def test():
+    a=a+10  #it gives error
+    print(a)
+    return
+test()'''
+
